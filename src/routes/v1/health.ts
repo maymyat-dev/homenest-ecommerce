481f8fa58b140db165005700f41869368f12bd22
mmm-maymyatmon <maymyat.dev@gmail.com>
2025-12-17 12:56:11 +0700

    delete health.ts

--- src/routes/v1/health.ts
+++
@@ -1,9 +0,0 @@
-import express from "express"
-import { check } from "../../middlewares/check";
-import { healthCheck } from "../../controllers/healthController";
-
-const router = express.Router();
-
-router.get("/health", check, healthCheck )
-
-export default router;
