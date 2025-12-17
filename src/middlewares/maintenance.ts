490218cc68d2e0f36ede45da0c6f112dfe46c407
mmm-maymyatmon <maymyat.dev@gmail.com>
2025-12-17 16:02:32 +0700

    change maintenance.ts

--- src/middlewares/maintenance.ts
+++ src/middlewares/maintenance.ts
@@ -3,29 +3,35 @@ import { getSettingStatus } from "../services/settingService";
 import { errorCode } from "../../config/errorCode";
 import { createError } from "../utils/error";
 
-const whiteLists = ["127.0.0.1"];
+const whiteLists = ["127.0.0.1", "::1", "::ffff:127.0.0.1"];
 export const maintenance = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  const ip: any = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
-  if (whiteLists.includes(ip)) {
-    console.log(`Allowed IP: ${ip}`);
-    next();
-  } else {
-    console.log(`Not Allowed IP: ${ip}`);
 
+  const ip = (req.headers["x-forwarded-for"] as string) || req.socket.remoteAddress || "";
+
+
+  if (whiteLists.some(allowedIp => ip.includes(allowedIp))) {
+    return next();
+  }
+
+  try {
     const setting = await getSettingStatus("maintenance");
     if (setting?.value === "true") {
+  
       return next(
         createError(
           "This server is currently under maintenance. Please try again later.",
-          401,
+          503, 
           errorCode.maintenance
         )
       );
     }
+    next();
+  } catch (error) {
+   
     next();
   }
 };
