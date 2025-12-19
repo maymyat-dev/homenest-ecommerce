098e30da5dd23ae86d8fa59b8969c4fc8ce0152a
mmm-maymyatmon <maymyat.dev@gmail.com>
2025-12-19 13:58:30 +0700

    delete Cross-Origin-Resource-Policy, same-site

--- src/app.ts
+++ src/app.ts
@@ -78,10 +78,10 @@ i18next
 
 app.use(middleware.handle(i18next));
 
-app.use((req, res, next) => {
-  res.setHeader("Cross-Origin-Resource-Policy", "same-site");
-  next();
-})
+// app.use((req, res, next) => {
+//   res.setHeader("Cross-Origin-Resource-Policy", "same-site");
+//   next();
+// })
 
 app.use(express.static("public"));
 app.use(express.static("uploads"));
