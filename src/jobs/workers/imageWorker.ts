78a28cbd9e5d5b54dbffb6e90a9fcd0e7745802e
mmm-maymyatmon <maymyat.dev@gmail.com>
2025-12-19 14:39:52 +0700

    Fix prisma and cors

--- src/jobs/workers/imageWorker.ts
+++ src/jobs/workers/imageWorker.ts
@@ -1,53 +1,53 @@
-import { Worker } from "bullmq";
-import { redis } from "../../../config/redisClient";
-import sharp from "sharp";
-import { v2 as cloudinary } from "cloudinary";
-import { prisma } from "../../services/prismaClient";
-import dotenv from "dotenv";
-dotenv.config();
+// import { Worker } from "bullmq";
+// import { redis } from "../../../config/redisClient";
+// import sharp from "sharp";
+// import { v2 as cloudinary } from "cloudinary";
+// import { prisma } from "../../services/prismaClient";
+// import dotenv from "dotenv";
+// dotenv.config();
 
 
 
-const imageWorker = new Worker(
-  "imageQueue",
-  async (job) => {
-    try {
-      const { filePath, imageId, postId, width, height, quality = 80 } = job.data;
+// const imageWorker = new Worker(
+//   "imageQueue",
+//   async (job) => {
+//     try {
+//       const { filePath, imageId, postId, width, height, quality = 80 } = job.data;
 
-      if (!filePath) throw new Error("filePath missing");
+//       if (!filePath) throw new Error("filePath missing");
 
-      const optimizedBuffer = await sharp(filePath)
-        .resize(width || undefined, height || undefined)
-        .webp({ quality })
-        .toBuffer();
+//       const optimizedBuffer = await sharp(filePath)
+//         .resize(width || undefined, height || undefined)
+//         .webp({ quality })
+//         .toBuffer();
 
-      const uploadResult: any = await new Promise((resolve, reject) => {
-        cloudinary.uploader.upload_stream(
-          { folder: "homenest_uploads" },
-          (error, result) => {
-            if (error) reject(error);
-            else resolve(result);
-          }
-        ).end(optimizedBuffer);
-      });
+//       const uploadResult: any = await new Promise((resolve, reject) => {
+//         cloudinary.uploader.upload_stream(
+//           { folder: "homenest_uploads" },
+//           (error, result) => {
+//             if (error) reject(error);
+//             else resolve(result);
+//           }
+//         ).end(optimizedBuffer);
+//       });
 
-      const cloudinaryUrl = uploadResult.secure_url;
-      if (!cloudinaryUrl) throw new Error("Cloudinary URL missing");
+//       const cloudinaryUrl = uploadResult.secure_url;
+//       if (!cloudinaryUrl) throw new Error("Cloudinary URL missing");
 
-      if (postId) {
-        await prisma.post.update({
-          where: { id: Number(postId) },
-          data: { image: cloudinaryUrl },
-        });
-      } else if (imageId) {
-        await prisma.image.update({
-          where: { id: Number(imageId) },
-          data: { path: cloudinaryUrl },
-        });
-      }
-    } catch (error) {
-      console.error("Image Worker Failed ❌", error);
-    }
-  },
-  { connection: redis }
-);
+//       if (postId) {
+//         await prisma.post.update({
+//           where: { id: Number(postId) },
+//           data: { image: cloudinaryUrl },
+//         });
+//       } else if (imageId) {
+//         await prisma.image.update({
+//           where: { id: Number(imageId) },
+//           data: { path: cloudinaryUrl },
+//         });
+//       }
+//     } catch (error) {
+//       console.error("Image Worker Failed ❌", error);
+//     }
+//   },
+//   { connection: redis }
+// );
