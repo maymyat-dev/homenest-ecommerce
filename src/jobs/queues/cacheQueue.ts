0062d341ab777af31f498654c099a1a10b995d26
mmm-maymyatmon <maymyat.dev@gmail.com>
2025-12-20 07:48:08 +0700

    add cahceQueue consloe log

--- src/jobs/queues/cacheQueue.ts
+++ src/jobs/queues/cacheQueue.ts
@@ -1,6 +1,7 @@
 import { qstash } from "../../utils/qstash";
 
 export async function enqueueCacheInvalidation(data: any) {
+  console.log("Attempting to publish to QStash...", data);
   await qstash.publishJSON({
     url: "https://homenest.backend.maymyatmon.com/api/v1/cache/invalidate",
     body: data,
