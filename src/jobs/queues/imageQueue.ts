1fecf1bede97875b57a3e70a3325494aecb8b293
mmm-maymyatmon <maymyat.dev@gmail.com>
2025-12-20 08:21:10 +0700

    change route url images to image

--- src/jobs/queues/imageQueue.ts
+++ src/jobs/queues/imageQueue.ts
@@ -3,7 +3,7 @@ import { qstash } from "../../utils/qstashClient";
 
 export async function enqueueImageJob(data: any) {
   return await qstash.publishJSON({
-    url: "https://homenest.backend.maymyatmon.com/api/v1/qstash/images/process",
+    url: "https://homenest.backend.maymyatmon.com/api/v1/qstash/image/process",
     body: data,
   });
 }
